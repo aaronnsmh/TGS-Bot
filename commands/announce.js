6946c7e84017c76c8ba2a5ff34db4bb8d32fa5f3
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-29 20:21:24 +0100

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -1,30 +1,20 @@
 exports.run = (Discord, client, message, args) => {
 
-let allowedRole = message.guild.roles.find("name", "bot admin");	
-let thumbnail = client.user.avatarURL;
-let name = client.user.username;
-
-
- if(message.member.roles.has(allowedRole.id)) {
+	
+if (message.member.hasPermission("ADMINISTRATOR")) {
 		   const color = args[0]
 				
 		   const text = args.slice(1).join(" ");
 		   const author = message.author;
-		   if (text.length < 1) return message.channel.send("I can't announce nothing. Use the format: -announce [TITLE] [DESCRIPTION].");
+		   if (text.length < 1) return message.channel.send("Can not announce nothing");
 		   //const colour = args.slice(2).join("");
 		   const embed = new Discord.RichEmbed()
-		   .setColor("#00ff00")
-		   .setThumbnail(thumbnail)
-		   .setAuthor(name)
+		   .setColor("#FF0000")
+		   .setThumbnail("https://cdn.discordapp.com/attachments/440213746820841472/440229522890293268/TGSSparklesBEZEL.png")
 		   .setTitle(":mega: Announcement:")
 		   .setDescription(text)
-		   .setFooter(name + "| Announced at ")
+		   .setFooter("An announcment from " + author + " made at ")
 		   .setTimestamp()
 		   message.channel.send("Announcement from: " + author);
 		   message.channel.send({embed})
-	   }
-else {
-	message.channel.send("You need to have the 'bot admin' role to announce!")
-	return;
-}
 }
