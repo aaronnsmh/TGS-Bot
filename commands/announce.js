0e3f67f4c56236506f80f0c1b468c8b5c035e150
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-30 16:36:57 +0100

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -11,7 +11,7 @@ exports.run = (Discord, client, message, args) => {
 		   .setColor("#00ff00")
 		   .setThumbnail(client.user.avatarURL)
 		   .setTitle(":mega: Announcement: " + title)
-		   .setDescription("**Announced by: " + message.author + "**\n" + text)
+		   .setDescription("**Announced by: " + message.author + "**\n\n\n" + text)
 		   .setFooter("An announcment from" + author + " made at ")
 		   .setTimestamp()
 		   message.channel.send({embed})
