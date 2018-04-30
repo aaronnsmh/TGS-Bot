1bfe4cba9ba66eb71210a212b789fa84ce4a42db
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-30 16:35:34 +0100

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -2,7 +2,7 @@ exports.run = (Discord, client, message, args) => {
 
        if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("You need the ADMINISTRATOR permission to run this comamnd!")
 		   const color = args[0]
-				
+		   let title = args[0];
 		   const text = args.slice(1).join(" ");
 		   const author = message.author;
 		   if (text.length < 1) return message.channel.send("Can not announce nothing");
@@ -10,7 +10,7 @@ exports.run = (Discord, client, message, args) => {
 		   const embed = new Discord.RichEmbed()
 		   .setColor("#00ff00")
 		   .setThumbnail(client.user.avatarURL)
-		   .setTitle(":mega: Announcement:")
+		   .setTitle(":mega: Announcement: " + title)
 		   .setDescription("**Announced by: ${message.author}**\n" + text)
 		   .setFooter("An announcment from" + author + " made at ")
 		   .setTimestamp()
