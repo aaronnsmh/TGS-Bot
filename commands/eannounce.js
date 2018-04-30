4e22fb3b0c2882d314747df06e89044feb8be8b4
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-30 16:41:26 +0100

    Update eannounce.js

--- commands/eannounce.js
+++ commands/eannounce.js
@@ -2,19 +2,20 @@ exports.run = (Discord, client, message, args) => {
 
        if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("You need the ADMINISTRATOR permission to run this comamnd!")
 		   const color = args[0]
-				
+		   let title = args[0];
 		   const text = args.slice(1).join(" ");
 		   const author = message.author;
 		   if (text.length < 1) return message.channel.send("Can not announce nothing");
 		   //const colour = args.slice(2).join("");
 		   const embed = new Discord.RichEmbed()
 		   .setColor("#00ff00")
-		   .setThumbnail("https://t6.rbxcdn.com/fad4c603a443958512e8adb3660ff221")
-		   .setTitle(":mega: Announcement:")
-		   .setDescription(text)
-		   .setFooter("An announcment from " + author + " made at ")
+		   .setThumbnail(client.user.avatarURL)
+		   .setTitle(":mega: Announcement: " + title)
+		   .setDescription("**Announced by: " + message.author + "**\n\n" + text + "\n")
+		   .setFooter("An announcment made at ")
 		   .setTimestamp()
-		   message.channel.send("@everyone Announcement from: " + author);
+		   message.delete(10)
+		   message.channel.send("@everyone")
 		   message.channel.send({embed})
 	   
 }
