92dda6bf2a0044a22a80671958281bf462e4329f
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-07 14:37:12 +0100

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -3,9 +3,8 @@ const Discord = require('discord.js')
 exports.run = async (Discord, client, message, args) => {
   if (!args) return message.reply("You must have something to vote for!")
   if (!message.content.includes("?")) return message.reply("Include a ? in your vote!")
-    message.channel.send(`:ballot_box:  ${message.author.username} started a vote! React to my next message to vote on it. :ballot_box: `);
+    
   var admin= new Discord.RichEmbed()
-        .setFooter("© " + name + "  | Made by Aaron#1742")
         .setTitle("Vote: " + args)
         .setDescription("React below with ✅ or ⛔!")
         .setFooter("Vote requested by: " + message.author.id + " at: ")
