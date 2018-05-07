c3530b59b107ddf59edae69e2df9e684a3efd84c
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-07 14:31:00 +0100

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -4,16 +4,7 @@ exports.run = async (Discord, client, message, args) => {
   if (!args) return message.reply("You must have something to vote for!")
   if (!message.content.includes("?")) return message.reply("Include a ? in your vote!")
     message.channel.send(`:ballot_box:  ${message.author.username} started a vote! React to my next message to vote on it. :ballot_box: `);
-    const pollTopic = await message.channel.send({
-		embed: {
-			author: { name: `${message.author.id}'` },
-			fields: [{
-				name: "Vote on:",
-				value: args,
-				inline: true
-			}
-		}
-});
+    const pollTopic = await message.channel.send(args);
     pollTopic.react(`✅`);
     pollTopic.react(`⛔`);
 };
