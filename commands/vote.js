71d548f7865c4896431b2b7bd63a3dba871d6207
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-07 14:25:22 +0100

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js')
 
-exports.run = (Discord, client, message, args) => {
+exports.run = async (Discord, client, message, args) => {
   if (!args) return message.reply("You must have something to vote for!")
   if (!message.content.includes("?")) return message.reply("Include a ? in your vote!")
     message.channel.send(`:ballot_box:  ${message.author.username} started a vote! React to my next message to vote on it. :ballot_box: `);
