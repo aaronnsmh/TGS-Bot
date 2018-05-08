2cddf584d73b124cac8bb4a04580166c5dde58ed
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-08 17:34:47 +0100

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -3,7 +3,11 @@ const Discord = require('discord.js')
 exports.run = async (Discord, client, message, args) => {
   if (!args) return message.reply("You must have something to vote for!")
   if (!message.content.includes("?")) return message.reply("Include a ? in your vote!")
-    
+    message.channel.send("Creating Vote.")
+    message.edit("Creating Vote..")
+    message.edit("Creating Vote...")
+    message.edit("Vote Created! React Below.")
+    message.delete(30)
   var admin= new Discord.RichEmbed()
         .setTitle("Vote: " + args.slice(0).join(" "))
         .setDescription("React below with ✅ or ⛔!")
