8c2311c9a9ebf5acbed1a71ccd085ec8e1a5c97a
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-09 19:29:39 +0100

    Update setprefix.js

--- commands/setprefix.js
+++ commands/setprefix.js
@@ -8,7 +8,14 @@ exports.run = (Discord, client, message, args) => {
     db.set(`guildPrefix_${message.guild.id}`, args.join().trim()).then(i => {
 
         message.channel.send("**Prefix Changed To: **" + i)
-
+         let staffc = message.guild.channels.find("name", "logs")
+        
+        if (!staffc) return message.reply('I cannot find a #logs channel, please ask a server admin to make one in order to use moderation/admin commands!');
+        const embed = new Discord.RichEmbed()
+            .setColor(0x8cff00)
+            .setTimestamp()
+            .setDescription(`**Action:** SetPrefix\n**User:** ${message.author.tag}\n**They changed it to:** ${i});
+         staffc.send({embed});
     })
 
 }
