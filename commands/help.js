f429897ffa05a31929f267c9172d569ef552dea0
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-09 19:51:28 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,19 +1,19 @@
 var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-
+const db = require('quick.db')
 exports.run = (Discord, client, message, args) => {
     
-let thumbnail = client.user.avatarURL;
-let name = client.user.username;
-    
-    message.reply("Here is the help you are looking for:");
-    var embedcmds= new Discord.RichEmbed()
-        .setFooter("© Made by Aaron")
-        .setColor(randomColor)
-        .setTitle("Bot Help")
-        .setDescription("The prefix for this server is '!!'. Run !!cmds for info on how to use each command!")
-        .setThumbnail(thumbnail)
-        .setAuthor(name,thumbnail)
-    message.channel.sendEmbed(embedcmds);
-    message.delete(10000)
+    let thumbnail = client.user.avatarURL;
+    let name = client.user.username;
+    db.fetch(guildPrefix_${message.guild.id}, args.join().trim()).then(i => {
+        message.reply("Here is the help you are looking for:");
+        var embedcmds= new Discord.RichEmbed()
+            .setFooter("© Made by Aaron")
+            .setColor(randomColor)
+            .setTitle("Bot Help")
+            .setDescription("The prefix for this server is '" + i + "'. Run " + i + "cmds for info on how to use each command!")
+            .setThumbnail(thumbnail)
+            .setAuthor(name,thumbnail)
+        message.channel.sendEmbed(embedcmds);
+        message.delete(10000)
 
 }
